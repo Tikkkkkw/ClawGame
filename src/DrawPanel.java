93f7c5980201e528bea6284660bc78eeefa3830f
BT_4N2_08 <G@F>
2024-04-16 14:02:30 -0400

    Created detection of the rectangles to the mouse click point. in DrawPanel associated clicking modes to setting them in GameMode Created plan to further organize between the game process

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,14 +1,9 @@
+import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.Point;
-import java.util.ArrayList;
-import java.awt.Font;
+import javax.swing.*;
 
 class DrawPanel extends JPanel implements MouseListener {
-
     private Rectangle backpack;
     private Rectangle achivement;
     private Rectangle setting;
@@ -20,38 +15,62 @@ class DrawPanel extends JPanel implements MouseListener {
     public DrawPanel() {
         backpack = new Rectangle(20, 10, 50,50);
         achivement = new Rectangle(80, 10, 50, 50);
-        setting = new Rectangle(1000-85, 10, 50, 50);
         sound = new Rectangle(1000-145, 10, 50, 50);
+        setting = new Rectangle(1000-85, 10, 50, 50);
         this.addMouseListener(this);
-
     }
 
     protected void paintComponent(Graphics g) {
         g.setFont(new Font("Courier New", Font.BOLD, 45));
+        // if intro screen is true
+            // drawIntroScreen(g);
         easy(g);
         medium(g);
         hard(g);
-        draw(backpack, g);
-        draw(achivement, g);
-        draw(setting, g);
-        draw(sound, g);
+        startingScreen(g);
+    }
+
+    private void drawIntroScreen(Graphics g) {
 
     }
 
     public void mousePressed(MouseEvent e) {
 
         Point clicked = e.getPoint();
-
+        GameMode GM = new GameMode();
+// left
         if (e.getButton() == 1) {
+            if (detectRectangle(easy, clicked)){
+                GM.setEasy();
+            }
+
+            if (detectRectangle(medium, clicked)) {
+                GM.setMedium();
+            }
+
+            if (detectRectangle(hard, clicked)){
+                GM.setHard();
+            }
+            if (detectRectangle(backpack,clicked)){
+                newScreen();
+                System.out.println("backpack opening");
+            }
+            if (detectRectangle(achivement,clicked)){
+                System.out.println("Achivements!");
+            }
+            if (detectRectangle(sound,clicked)){
+                System.out.println("sound change");
+            }
+            if (detectRectangle(setting,clicked)){
+                System.out.println("settings");
+            }
+
 
         }
-
+// right
         if (e.getButton() == 3) {
 
         }
-    }
-    public void drawRect(){
-
     }
     public void easy(Graphics g){
         g.drawString("EASY", 162, 200);
@@ -62,7 +81,6 @@ class DrawPanel extends JPanel implements MouseListener {
         g.drawString("MEDIUM", 140, 325);
         medium = new Rectangle(160-45, 325-39, 160+45, 50);
         draw(medium,g);
-
     }
     public void hard(Graphics g){
         g.drawString("HARD", 162, 450);
@@ -71,18 +89,35 @@ class DrawPanel extends JPanel implements MouseListener {
     }
 
     public void startingScreen(Graphics g){
-//        g.setFont(new Font("Courier New", Font.BOLD, 50));
-        g.drawString("EASY", 200, 200);
-        g.drawString("MEDIUM", 200, 300);
-        g.drawString("HARD", 200, 400);
+        g.drawString("B", backpack.x,50);
+        g.drawString("A", achivement.x, 50);
+        g.drawString("L", sound.x, 50);
+        g.drawString("S", setting.x, 50);
+        draw(backpack, g);
+        draw(achivement, g);
+        draw(sound, g);
+        draw(setting, g);
     }
 
 
+    public void newScreen() {
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(300, 310));
+        layeredPane.setBorder(BorderFactory.createEmptyBorder());
+
+
+
+    }
 
     public void mouseReleased(MouseEvent e) { }
     public void mouseEntered(MouseEvent e) { }
     public void mouseExited(MouseEvent e) { }
     public void mouseClicked(MouseEvent e) { }
+
+
+
+
 
 
 
@@ -93,7 +128,14 @@ class DrawPanel extends JPanel implements MouseListener {
         int height = (int)r.getHeight();
         int width = (int)r.getWidth();
         g.drawRect(x, y, width, height);
-
-
     }
+
+    public boolean detectRectangle(Rectangle r, Point mouse){
+        boolean clicked = false;
+        if( (mouse.x >= r.x && mouse.x <= r.x+r.width) && (mouse.y >= r.y && mouse.y <= r.y+r.height) ){
+            clicked = true;
+        }
+        return clicked;
+    }
+
 }
--- src/GameMode.java
+++ src/GameMode.java
@@ -1,13 +1,60 @@
 import java.util.ArrayList;
 
 public class GameMode {
-    boolean mode;
+    String mode;
+
+    boolean easy;
+    boolean medium;
+    boolean hard;
     boolean pickedup;
     boolean solved;
     ArrayList<String> boxLock;
 
-    public GameMode(boolean mode){
-        this.mode = mode;
+   public GameMode(){
+   }
+
+    public String getMode() {
+        return mode;
     }
 
+
+
+    public void setEasy() {
+        easy = true;
+        medium = false;
+        hard = false;
+        System.out.println("Game set to easy");
+    }
+
+    public void setMedium() {
+        medium = true;
+        easy = false;
+        hard = false;
+        System.out.println("Game set to medium");
+    }
+
+    public void setHard() {
+        hard = true;
+        easy = false;
+        medium = false;
+        System.out.println("Game set to hard");
+    }
+
+
+
+    public boolean isPickedup() {
+        return pickedup;
+    }
+
+    public void setPickedup(boolean pickedup) {
+        this.pickedup = pickedup;
+    }
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
+    }
 }
